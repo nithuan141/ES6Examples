4b56317027c9283801d30926b2bd555317ffb71e
Nithesh AN <nithuan141@gmail.com>
2015-11-03 14:01:07 +0000

    Create ESClassExample_transpiled.js

---
+++ Class/ESClassExample_transpiled.js
@@ -0,0 +1,25 @@
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var Bake = (function () {
+  function Bake(cake) {
+    _classCallCheck(this, Bake);
+
+    this.cake = cake;
+  }
+
+  _createClass(Bake, [{
+    key: 'getTheCake',
+    value: function getTheCake() {
+      console.log(this.cake + ' ');
+    }
+  }]);
+
+  return Bake;
+})();
+
+var a = new Bake('Plum cake');
+a.getTheCake();
+a.cake='Choclate cake';
+a.getTheCake();
