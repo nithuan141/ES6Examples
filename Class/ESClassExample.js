eda0c7069eca6455a27fc1a0028b3f7868d7741c
Nithesh AN <nithuan141@gmail.com>
2018-08-22 10:13:52 +0530

    Update ESClassExample.js

--- Class/ESClassExample.js
+++ Class/ESClassExample.js
@@ -1,11 +1,14 @@
 class Bake {
-constructor(cake) {
-this.cake = cake;
+  constructor(cake) {
+    this.cake = cake;
+  }
+  
+  getTheCake() {
+    console.log(`${this.cake}`);
+  }
 }
-getTheCake() {
-console.log(`${this.cake}`);
-}
-}
+
+/*Creating the object of Bake*/
 var a = new Bake('Plum cake');
 a.getTheCake();
 a.cake='Choclate cake';
